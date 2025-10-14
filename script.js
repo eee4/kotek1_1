7be80066ba42e6fb30d10f6d4c424f254b9f0dd1
eee4 <41441600+eee4@users.noreply.github.com>
2025-10-14 12:00:10 +0200

    Com1_1.2

--- script.js
+++ script.js
@@ -6,10 +6,28 @@ function piszLiczby09() {
   poleTekstowe.innerText = tablicaZLiczbami.join(", ");
 }
 
+function weryfikujNrTelefonu(evt) {
+  const poleNrTelefonuText = document.getElementById("ex2_content");
+  // - długość ≠ 9 → komunikat: „Długość numeru musi być równa 9”
+  if (evt.currentTarget.value.length != 9) poleNrTelefonuText.innerText = "Długość numeru musi być równa 9";
+  // - zawiera litery → „Numer nie może zawierać liter”
+  else if (/([a-zA-Zęśąćźżłóń])+/.exec(evt.currentTarget.value) !== null) poleNrTelefonuText.innerText = "Numer nie może zawierać liter";
+  // - zawiera znaki specjalne → „Numer nie może zawierać znaków specjalnych”
+  else if (/([\+\-*()\[\]&^%$#@!{};:'",<.>\/?\\\|`~_=])+/.exec(evt.currentTarget.value) !== null)  poleNrTelefonuText.innerText = "Numer nie może zawierać znaków specjalnych";
+  // - dokładnie 9 cyfr → „Numer telefonu jest poprawny”
+  else if (/^(\d){9}$/.exec(evt.currentTarget.value) !== null && evt.currentTarget.value.length == 9) poleNrTelefonuText.innerText = "Numer telefonu jest poprawny";
+  // w przeciwnym, nieprzewidzianym przypadku
+  else poleNrTelefonuText.innerText = "Ten numer telefonu jest nieprawidłowy";
+}
+
 (function () {
 
-  // Zad. 1_1: liczby 0-9
+  // Zad. 1_1.1: liczby 0-9
   const przycisk = document.getElementById("ex1_button");
   przycisk.addEventListener("click", piszLiczby09);
+
+  // Zad. 1_1.2: weryfikacja numeru telefonu
+  const poleNrTelefonu = document.getElementById("ex2_text");
+  poleNrTelefonu.addEventListener("change", weryfikujNrTelefonu);
 
 })();
