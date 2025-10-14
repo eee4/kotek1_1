28582fc8e8a8ddf6011f40b5821aebf03176c8f7
eee4 <41441600+eee4@users.noreply.github.com>
2025-10-14 11:18:21 +0200

    Com1_1.1

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+
+function piszLiczby09() {
+  const poleTekstowe = document.getElementById("ex1_content");
+  var tablicaZLiczbami = [];
+  for (let i = 0; i <= 9; i++) tablicaZLiczbami.push(i);
+  poleTekstowe.innerText = tablicaZLiczbami.join(", ");
+}
+
 (function () {
-  //TODO
+
+  // Zad. 1_1: liczby 0-9
+  const przycisk = document.getElementById("ex1_button");
+  przycisk.addEventListener("click", piszLiczby09);
+
 })();
